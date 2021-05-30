40236feef0b8ab3d7af7f63fda65b128f8f6c77b
arcbjorn <arky.happ@gmail.com>
2021-05-30 20:13:01 +0300

    feat (sway): update script
     - remove kitty, add foot
     - add env variables for Wayland init
     - add pyton-pywal
     - add pcmanfm-qt
     - add pass
     - add neofetch
     - add dunst

--- sway.sh
+++ sway.sh
@@ -3,7 +3,7 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo reflector -c Russia -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syy
 
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
@@ -13,25 +13,49 @@ sudo firewall-cmd --reload
 
 echo "MAIN PACKAGES FOR SWAY"
 
-# install Window Manager Sway, App Launcher Wofi, App for theme customization, Status Bar, Terminal emulator
-sudo pacman -S sway wofi lxappearance waybar kitty
+# install DE packages from AUR:
 
-# install Login Manager LY
-paru -S --noconfirm ly-git
+# Window Manager Sway (sway-borders fork)
+# Login Manager
+# Web Browser
+# System backup & restore tool
+
+paru -S --noconfirm sway-borders-git ly-git firefox-nightly timeshift timeshift-autosnap
+
+# install DE packages from offical repos:
+
+# App Launcher
+# Theme customization package
+# Theme color generator
+# Status Bar
+# File manager
+# Password manager
+# Terminal emulator
+# CLI to display system specs
+# System notifications manager
+
+sudo pacman -S wofi lxappearance python-pywal waybar pcmanfm-qt pass foot neofetch dunst
 
 # enable login screen on boot
 sudo systemctl enable ly.service
 
-mkdir -p .config/{sway,dunst,ly,waybar,kitty}
+mkdir -p .config/{sway,dunst,waybar,wofi}
 
 cp /etc/sway/config ~/.config/sway/
 cp /etc/dunst/dunstrc ~/.config/dunst/
-cp /etc/ly/config.ini ~/.config/ly/
+cp /usr/share/foot/foot.ini ~/.config/foot/
 
-touch~/.config/waybar/config
-touch ~/.config/kitty/kitty.conf
+touch ~/.config/waybar/config
+touch ~/.config/wofi/config
 
-# install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
-# install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
+# cp /etc/ly/config.ini ~/.config/ly/
+# config for ly: /etc/ly/config.ini
 
-printf "\e[1;32mCHANGE NECESSARY FILES BEFORE REBOOT\e[0m"
+# set environment variables to use Wayland
+echo "QT_QPA_PLATFORM=wayland" >> /etc/environment
+echo "MOZ_ENABLE_WAYLAND=1" >> /etc/environment
+echo "MOZ_WEBRENDER=1" >> /etc/environment
+echo "XDG_SESSION_TYPE=wayland" >> /etc/environment
+echo "XDG_CURRENT_DESKTOP=sway" >> /etc/environment
+
+printf "\e[1;32mCHANGE NECESSARY FILES BEFORE REBOOT, THEN RUN REBOOT COMMAND\e[0m"
